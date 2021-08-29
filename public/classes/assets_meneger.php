26605c0a78389869eb281b6ff19371766e1cb962
AbmSourav <keramotul.islam@gmail.com>
2021-08-29 14:06:25 +0600

    conditional assets loading removed

--- public/classes/assets_meneger.php
+++ public/classes/assets_meneger.php
@@ -19,42 +19,30 @@ class Assets_Manager {
     }
 
     public static function styles() {
-		global $post;
-		
-		if ( isset( $post->post_content ) && has_shortcode( $post->post_content, 'talash-search' ) ) {
 			wp_enqueue_style( 'talash-daterangepicker-css', TALASH_URL . 'assets/vendors/date-range-picker/daterangepicker.min.css', array(), TALASH_VERSION, 'all'  );
 			wp_enqueue_style( 'talash-main-css', TALASH_URL . 'assets/css/talash-main.css', array(), TALASH_VERSION, 'all'  );
-		}
     }
 
 	public static function scripts() {
-		global $post;
-		
-		if ( isset( $post->post_content ) && has_shortcode( $post->post_content, 'talash-search' ) ) {
-			wp_enqueue_script( 'talash-moment', TALASH_URL . 'assets/vendors/date-range-picker/moment.min.js', array( 'jquery' ), TALASH_VERSION, true  );
-			wp_enqueue_script( 'talash-daterangepicker', TALASH_URL . 'assets/vendors/date-range-picker/daterangepicker.js', array( 'jquery' ), TALASH_VERSION, true  );
-		
-			if ( TALASH_ENV_DEV ) {
-				wp_enqueue_script( 'talash-main', TALASH_URL . 'assets/js/talash-main.js', array( 'jquery' ), TALASH_VERSION . time(), true  );
-			} else {
-				wp_enqueue_script( 'talash-main', TALASH_URL . 'assets/js/talash-main.min.js', array( 'jquery' ), TALASH_VERSION, true  );
-			}
+		wp_enqueue_script( 'talash-moment', TALASH_URL . 'assets/vendors/date-range-picker/moment.min.js', array( 'jquery' ), TALASH_VERSION, true  );
+		wp_enqueue_script( 'talash-daterangepicker', TALASH_URL . 'assets/vendors/date-range-picker/daterangepicker.js', array( 'jquery' ), TALASH_VERSION, true  );
+	
+		if ( TALASH_ENV_DEV ) {
+			wp_enqueue_script( 'talash-main', TALASH_URL . 'assets/js/talash-main.js', array( 'jquery' ), TALASH_VERSION . time(), true  );
+		} else {
+			wp_enqueue_script( 'talash-main', TALASH_URL . 'assets/js/talash-main.min.js', array( 'jquery' ), TALASH_VERSION, true  );
 		}
     }
 
 	public static function public_api() {
-		global $post;
-		
-		if ( isset( $post->post_content ) && has_shortcode( $post->post_content, 'talash-search' ) ) {
-			wp_localize_script( 
-				'talash-main',
-				'talashPublicApi',
-				[
-					'ajax_url'	=> admin_url( 'admin-ajax.php' ),
-					'nonce'		=> wp_create_nonce( 'talash_public_nonce' ),
-				]
-			);
-		}
+		wp_localize_script( 
+			'talash-main',
+			'talashPublicApi',
+			[
+				'ajax_url'	=> admin_url( 'admin-ajax.php' ),
+				'nonce'		=> wp_create_nonce( 'talash_public_nonce' ),
+			]
+		);
 	}
 
 }
